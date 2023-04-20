19ef47139f006e43be863462506fc9a96f618393
Arash Javanmardi <ajavanma@rhrk.uni-kl.de>
2023-04-20 22:18:04 +0200

    OyVwn

--- pointclouds/pcd_report.cpp
+++ pointclouds/pcd_report.cpp
@@ -22,7 +22,7 @@ void process_pcd_file(const fs::path &input_file, const fs::path &log_file_path)
         return;
     }
 
-    std::vector<std::string> field_names = {"x", "y", "z", "r", "g", "b", "normal_x", "normal_y", "normal_z"};
+    const std::vector<std::string> field_names = {"x", "y", "z", "r", "g", "b", "normal_x", "normal_y", "normal_z"};
     int num_fields = field_names.size();
 
     std::vector<int> counter_zeros(num_fields, 0);
@@ -33,7 +33,7 @@ void process_pcd_file(const fs::path &input_file, const fs::path &log_file_path)
 
     for (const auto &point : point_cloud->points)
     {
-        std::vector<double> point_data = {point.x, point.y, point.z, point.r, point.g, point.b, point.normal_x, point.normal_y, point.normal_z};
+        const std::vector<double> point_data = {point.x, point.y, point.z, point.r, point.g, point.b, point.normal_x, point.normal_y, point.normal_z};
 
         for (size_t i = 0; i < point_data.size(); ++i)
         {
@@ -80,3 +80,33 @@ void process_pcd_file(const fs::path &input_file, const fs::path &log_file_path)
     log_file << "PCD file information for '" << input_file << "':\n";
     log_file << point_cloud->size() << " points\n";
 
+    for (size_t i = 0; i < field_names.size(); ++i)
+    {
+        log_file << "Number of zeros found in " << field_names[i] << ": " << counter_zeros[i] << std::endl;
+        log_file << "Number of NaNs found in " << field_names[i] << ": " << counter_nans[i] << std::endl;
+        log_file << "Number of infinite values found in " << field_names[i] << ": " << counter_infs[i] << std::endl;
+        if (!std::isnan(min_values[i]) && !std::isnan(max_values[i])){
+            log_file << "Min and max values for " << field_names[i] << ": " << min_values[i] << ", " << max_values[i] << std::endl;
+        }
+    }
+    log_file << "Done writing to file" << std::endl;
+    log_file.close();
+}
+
+int main()
+{
+    constexpr fs::path pcd_files_path = "../data/old";
+    constexpr fs::path output_logs_path = "../logs";
+    for (const auto &entry : fs::directory_iterator(pcd_files_path))
+    {
+        if (entry.path().extension() == ".pcd")
+        {
+            fs::path input_file = entry.path();
+            fs::path log_file_path = output_logs_path / (input_file.stem().string() + "_report.log");
+
+            process_pcd_file(input_file, log_file_path);
+        }
+    }
+
+    return 0;
+}

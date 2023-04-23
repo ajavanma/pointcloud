45c7622d532b2a073bef15ae9776cdfa6bc4ce7e
Arash Javanmardi <ajavanma@rhrk.uni-kl.de>
2023-04-23 14:34:17 +0200

    6K93W

--- utils/_working_directory.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <filesystem>
-
-int main()
-{
-    std::filesystem::path current_working_dir = std::filesystem::current_path();
-    std::cout << "Current working directory: " << current_working_dir << std::endl;
-    return 0;
-}
